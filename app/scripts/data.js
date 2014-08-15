7e0395ace0b489a85dfda5423731bf8f6adfe06f
Jan Nonnen <jan.nonnen@viaboxx.de>
2014-08-15 14:14:08 +0200

    playing with drag and styles

---
+++ app/scripts/data.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+function retrievePlayer(){
+    return [
+        { 'name': 'Thomas Nicolaisen', 'rfid': '1' },
+        { 'name': 'Jan Nonnen', 'rfid': '2' },
+        { 'name': 'Roman Stumm', 'rfid': '3' },
+        { 'name': 'Jürgen Lüders', 'rfid': '4' },
+        { 'name': 'Simon Tiffert', 'rfid': '5' },
+        { 'name': 'Hussam Khuder', 'rfid': '6' }
+    ];
+}
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,49 @@
 'use strict';
 
+
+
 console.log('\'Allo \'Allo!');
 
-jQuery.getJSON('http://localhost:8080/players',function(data){
-    $('#playerList').empty();
-    $.each(data, function(_, element){
-        $('#playerList').append('<li>'+element.name+'</li>');
-    });
+var availablePlayers= retrievePlayer();
+
+
+var currentGame= {};
+
+
+
+$('#playerList').empty();
+
+
+availablePlayers.forEach(function(element){
+    $('#playerList').append( '<li class="label label-info playerName">'+element.name+'</li>');
+    $('.dropdownPlayer').append('<li role="presentation"><a role="menuitem" tabindex="-1" href="#">'+element.name+'</a></li>');
 });
 
+
+$('.playerName').draggable();
+
+$('.droppable').droppable({
+   drop: function( event, ui ) {
+    console.log( ui.draggable.text() );
+   },
+   over: function(event, ui) {
+       if($(ui.draggable).parent() !== $(this)){
+           $(ui.draggable).appendTo($(this));
+       }
+       console.log( 'over '+ui.draggable.text() );
+       $('ul', this).slideDown(100);
+   }
+  });
+
+
+$(function(){
+
+    $('.dropdown-menu li a').click(function(){
+
+
+        $('.btn:first-child').text($(this).text());
+        $('.btn:first-child').val($(this).text());
+
+    });
+
+});
