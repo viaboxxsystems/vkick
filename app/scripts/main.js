66ce1d541834889defdbf2ddf8bc69249d9a181b
Jan Nonnen <jan.nonnen@viaboxx.de>
2014-08-15 15:53:19 +0200

    game data from forms

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,9 +7,30 @@ console.log('\'Allo \'Allo!');
 var availablePlayers= retrievePlayer();
 
 
-var currentGame= {};
+/**
+ *
+ * {
+ *
+ *  team1 {
+ *      player1: '...',
+ *      player2: '...',
+ *      goals: 10
+ *   },
+ *   team2 {
+ *      player1: '...',
+ *      player2: '...'
+ *      goals: 4
+ *    },
+ *    timestamp: date()
+ *  }
+ *
+ */
 
 
+
+
+var goals1 = 0;
+var goals2 = 0;
 
 $('#playerList').empty();
 
@@ -20,30 +41,37 @@ availablePlayers.forEach(function(element){
 });
 
 
-$('.playerName').draggable();
+$('#goals1 .btn').click(function(){
+   goals1 = $(this).text();
+   $('#goals1 .btn').not(this).removeClass('clicked');
+   $(this).addClass( 'clicked' );
 
-$('.droppable').droppable({
-   drop: function( event, ui ) {
-    console.log( ui.draggable.text() );
-   },
-   over: function(event, ui) {
-       if($(ui.draggable).parent() !== $(this)){
-           $(ui.draggable).appendTo($(this));
-       }
-       console.log( 'over '+ui.draggable.text() );
-       $('ul', this).slideDown(100);
-   }
-  });
+});
 
 
+$('#goals2 .btn').click(function(){
+    goals2 = $(this).text();
+    $('#goals2 .btn').not(this).removeClass('clicked');
+    $(this).addClass( 'clicked' );
+
+});
 $(function(){
-
-    $('.dropdown-menu li a').click(function(){
-
-
-        $('.btn:first-child').text($(this).text());
-        $('.btn:first-child').val($(this).text());
-
+    $('.dropdown-menu').on('click', 'li a', function(){
+        $(this).parents('.dropdown').children('.dropdown-toggle').text($(this).text());
     });
 
+    $('.playedAction').click(function() {
+        var game = {
+            team1: {
+                player1: $('#player1').text(),
+                player2: $('#player2').text(),
+                goals: goals1
+            },
+            team2: {
+                player3: $('#player3').text(),
+                player4: $('#player4').text(),
+                goals: goals2
+            }
+        };
+    });
 });
