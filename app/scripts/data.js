397d184d4e86c3251e5d6725c574ab43864ddd4e
Jan Nonnen <jan.nonnen@viaboxx.de>
2014-11-03 23:18:08 +0100

    using backend for retrieving players

--- app/scripts/data.js
+++ app/scripts/data.js
@@ -1,15 +1,8 @@
 'use strict';
 
 
-function retrievePlayer(){
-    return [
-        { 'surname': 'Nicolaisen', 'name': 'Thomas', 'rfid': '1' },
-        { 'surname': 'Nonnen', 'name': 'Jan', 'rfid': '2' },
-        { 'surname': 'Stumm', 'name': 'Roman', 'rfid': '3' },
-        { 'surname': 'Tiffert', 'name': 'Simon', 'rfid': '4' },
-        { 'surname': 'Lüders', 'name': 'Jürgen', 'rfid': '5' },
-        { 'surname': 'Lutz', 'name': 'Olli', 'rfid': '6' }
-    ];
+function retrievePlayers(callback){
+    return jQuery.getJSON('http://localhost:9001/player',null, callback);
 }
 
 
